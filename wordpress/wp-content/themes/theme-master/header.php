fd9a687d186db9a2e6819ab3c98aa6b3291d6384
Halabi <dudih@sinapsa.co.il>
2013-01-05 22:55:03 +0200

    PLAY-19

--- wordpress/wp-content/themes/theme-master/header.php
+++ wordpress/wp-content/themes/theme-master/header.php
@@ -36,17 +36,24 @@
 </ul> -->
 	<div class="header-widget">
 		<div class="header-widget-contnet layout">
-			<h1 class="content-item">Content Item #1</h1>
-			<div class="details">
-				For developers, the Android 3.1 platform is
-				available as a downloadable component for
-				the Android SDK. The downloadable platform
-				includes an Android library and system
-				image, as well as a set of emulator skins and
-				more. The downloadable platform includes
-				no external libraries.
-			</div>
-			<a href="#" class="learn-more">Learn More</a>
-		</div>
+			<div class="left-side">
+<?php for($i=1; $i<=3; $i++) { ?>
+				<h1 class="content-item">Content Item #<?php echo $i ?></h1>
+				<div class="details">
+					For developers, the Android 3.1 platform is
+					available as a downloadable component for
+					the Android SDK. The downloadable platform
+					includes an Android library and system
+					image, as well as a set of emulator skins and
+					more. The downloadable platform includes
+					no external libraries.
+				</div>
+				<a href="#" class="learn-more">Learn More</a>			
+<?php } ?>	<!--End for-->
+			</div>	
+			<div class="right-side">
+				<img src="<?php echo bloginfo('template_directory'); ?>/images/theme/header/layer2/widget-pictures/widget-picture-01.png" />
+			</div>		
+		</div>			
 	</div>
 	<div class="header-widget-shadow"></div>
