d5d4e63277c8dfd9daba213a754f6851dc2eabd0
Halabi <dudih@sinapsa.co.il>
2013-01-08 02:03:45 +0200

    PLAY-19 footer was added

--- wordpress/wp-content/themes/theme-master/footer.php
+++ wordpress/wp-content/themes/theme-master/footer.php
@@ -1,6 +1,16 @@
 	<div class="footer layout">
-		<div class="left-side"><a href=""></a></div>
-		<div class="right-side">2222</div>
+		<div class="left-side"><a href="https://twitter.com/paul_irish"></a></div>
+		<div class="right-side">
+			<p>@Paul_Irish</p>
+			<nav>
+				<div>somebody had to write this. thank you.</div>
+				<div>there is no trivial way to know when they are loaded, unfortunately.</div>
+				<div>can you email me? holler@{myname}.com thx mucho. also just realized you're also the csshover.htc guy! so awesome! :)</div>
+				<div>airbnb probably has some great choices</div>
+				<div>yo man. i ended up getting a different place.. a studio.. so i don't think i can host you, unfortch.</div>
+				<div>more tweets...</div>
+			</nav>
+		</div>
 	</div>
 
 	<div id="footer">
